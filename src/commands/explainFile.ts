3c7fd3dd72b9a24aa72a5e239dffa466099a97bf
北慕白 <63459931+w1770946466@users.noreply.github.com>
2023-03-20 11:51:35 +0800

    Update explainFile.ts

--- src/commands/explainFile.ts
+++ src/commands/explainFile.ts
@@ -4,7 +4,7 @@ import { IEditor } from '../Editor'
 export const explainFileCommand = async (editor: IEditor, openAiApi: IOpenAIWrapper): Promise<void> => {
   const fileContents = editor.getCurrentFileContents()
   if (fileContents.length > 0) {
-    const prompt = `Explain the following ${editor.getCurrentFileExtension()} code: ${fileContents}`
+    const prompt = `解释 ${editor.getCurrentFileExtension()} code: ${fileContents}`
     const response = await openAiApi.makeRequestWithLoadingIndicator(prompt, editor)
     if (response !== undefined) {
       editor.writeToConsole(response)
