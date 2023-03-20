24f3fa382612db505d2e59c4ea9e80559e35cd9b
北慕白 <63459931+w1770946466@users.noreply.github.com>
2023-03-20 11:52:24 +0800

    Update explainSelection.ts

--- src/commands/explainSelection.ts
+++ src/commands/explainSelection.ts
@@ -4,7 +4,7 @@ import { IEditor } from '../Editor'
 export const explainSelectionCommand = async (editor: IEditor, openAiApi: IOpenAIWrapper): Promise<void> => {
   const highlighted = editor.getHighlightedText()
   if (highlighted.length > 0) {
-    const prompt = `Explain the following ${editor.getCurrentFileExtension()} code: ${highlighted}`
+    const prompt = `解释 ${editor.getCurrentFileExtension()} code: ${highlighted}`
     const response = await openAiApi.makeRequestWithLoadingIndicator(prompt, editor)
     if (response !== undefined) {
       editor.writeToConsole(response)
